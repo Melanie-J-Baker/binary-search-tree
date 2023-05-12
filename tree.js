1e8bd066b34fce671776f204599f963a73925056
Melanie-J-Baker <baker_mel@hotmail.com>
2023-05-12 09:20:28 +0100

    Correct find function. Write inorder and preorder functions

--- tree.js
+++ tree.js
@@ -29,7 +29,11 @@ export default class Tree {
     // insert function which accepts a value to insert
     insert (value, root = this.root) {
         if (root === null) return new Node(value);
-        root.data < value ? (root.right = this.insert(value, root.right)) : (root.left = this.insert(value, root.left));
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else if (value > root.data) {
+            root.right = insert(root.right, value);
+        }
         return root;
     }
 
@@ -43,7 +47,7 @@ export default class Tree {
     find (value, root = this.root) {
         if (root == null || root.data == value) return root;
         if (root.data < value) return this.find(value, root.right);
-        return this.find(root.left, value);
+        return this.find(value, root.left);
     }
 
     /* levelOrder function which accepts another function as a parameter.
@@ -52,7 +56,6 @@ export default class Tree {
     Tip: Use an array acting as a queue to keep track of all child nodes that you have yet to traverse and to add new ones to list (Video! - Binary tree - Level Order Traversal) 
     As we visit a node, can reference all of its children in a queue so we can visit them later*/
     levelOrder (callback) {
-
         // Start with address of root node in queue
         // As long as queue has at least one discovered node, we can take out a node from the front, visit it, and then enqueue it's children
         // Visit the root
@@ -64,28 +67,25 @@ export default class Tree {
     /* inorder, preorder, and postorder functions that accept a function parameter. 
     Each should traverse tree in their respective depth-first order and yield each node to provided function given as an argument. Functions shouold return an array of values if no function is given */
     // left root right - gives you a sorted list
-    inorder (node = this.root, callback, result = []) {
-
+    inorder (root = this.root, result = []) {
+        if (root === null) return;
+        if (root.left) this.inorder(root.left, result);
+        result.push(root.data);
+        if (root.right) this.inorder(root.right, result);
     }
     // root left right - for each node read data, go left until no more left - then go up and right
-    preorder (callback) {
-        if (!this.root) return [];
-        const stack = [this.root];
-        const results = [];
-        while (stack.length) {
-          const node = stack.pop();
-          if (node.right) stack.push(node.right);
-          if (node.left) stack.push(node.left);
-          if (callback) callback(node);
-          results.push(node.data);
-        }
-        if (!callback) return results;
-
-    }
+    preorder (root = this.root, result = []) {
+        if (root === null) return;
+        result.push(root.data);
+        if (root.left) this.predorder(root.left, result);
+        if (root.right) this.preorder(root.right, result);
+        return result;
+    };
+        
     // left right root
     postorder (callback) {
 
-    }
+    };
 
     // height function which accepts a node and returns its height (defined as no of edges in longest path from a given node to a leaf node)
     height (node = this.root) {
@@ -98,16 +98,16 @@ export default class Tree {
     // depth function which accepts a node and returns its depth (defined as the no of edges in path from a given node tree's root node)
     depth (node, root = this.root, level = 0) {
 
-    }
+    };
 
     // isBalanced function which checks if tree is balanced (difference between heights of left subtree and right subtree of every node is not more than 1)
     isBalanced (node = this.root) {
 
-    }
+    };
 
     // rebalance function which rebalances an unbalanced tree. Tip: Use a traversal method to provide a new array to the buildTree function
     rebalance () {
 
-    }
+    };
 }
 
